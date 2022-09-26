ec8c2bf2ef905db803e0a40fb153a4ac662a7735
isaac-ReDev <isacbs2017ce@gmail.com>
2022-09-26 10:34:53 -0300

    Diretorio dos components foi criado, e o component login

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { LogIn } from './Components/LogIn'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Lonin Page</h1>
+      <LogIn />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/LogIn/index.tsx
@@ -0,0 +1,22 @@
+
+export const LogIn = () => {
+
+    return(
+        <div>
+            <form>
+                <div>
+                    <label>Name</label>
+                    <input type="text" name="" id="" />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="" id="" />
+                </div>
+                <div>
+                    <label>Confirm Email</label>
+                    <input type="email" name="" id="" />
+                </div>
+            </form>
+        </div>
+    )
+};
